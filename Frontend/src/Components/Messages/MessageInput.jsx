6a9d513ea887349007fc0d099dd946f1369af4e6
sanket <sanketkopnar1299@gmail.com>
2024-02-24 14:48:27 +0530

    User can send message functionality done

--- Frontend/src/Components/Messages/MessageInput.jsx
+++ Frontend/src/Components/Messages/MessageInput.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
 import { BsSend } from "react-icons/bs";
+import {useSelector} from 'react-redux'
 export const MessageInput = () => {
+const [mssg,setmssg]=useState()
+const {users}=useSelector((state)=>state.usersdata)
+// console.log(users)
+// console.log("mssg",mssg)
+   const onHandleSendBtn=async(e)=>{
+      e.preventDefault()
+      try {
+         const res=await fetch(`/users/send/send_message/${users?._id}`,{
+            method:'POST',
+            headers:{
+               'Content-Type':'application/json'
+            },
+            body:JSON.stringify({message:mssg})
+         })
 
-
+         if(res.ok){
+            const data=await res.json()
+            console.log("data",data)
+         }
+         setmssg("")
+         
+      } catch (error) {
+         console.log("error at send messg in Messg IP Comp",error)
+      }
+   }
 
    return (
       <>
@@ -9,11 +34,14 @@ export const MessageInput = () => {
             <div className="w-full">
                <input
                   type="text"
+                  value={mssg}
+                  onChange={(e)=>setmssg(e.target.value)}
                   className="border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white"
                   placeholder="Send a message"
                />
                <button
                   type="submit"
+                  onClick={onHandleSendBtn}
                   className="absolute right-[20px] bottom-[-3px] inset-y-0 end-0 flex items-center pe-3"
                >
                   <BsSend />
--- Frontend/src/Components/Messages/NoChatSelected.jsx
+++ Frontend/src/Components/Messages/NoChatSelected.jsx
@@ -1,11 +1,14 @@
 import { TiMessages } from "react-icons/ti";
+import {useSelector} from 'react-redux'
 export const NoChatSelected = () => {
+  const {currentUser}=useSelector((state)=>state.userdata)
+  console.log(currentUser)
   return (
    <>
    <div className='flex items-center justify-center w-full h-full'>
 			<div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
 				<p>Welcome 👋  ❄</p>
-                {/* {authUser.fullName} */}
+                {currentUser?.data?.user?.fullName}
 				<p>Select a chat to start messaging</p>
 				<TiMessages className='text-3xl md:text-6xl text-center' />
 			</div>
